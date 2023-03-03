e0300f68d9cb4f24cfde7a843b6d4fb165d54ff6
Guilherme Lima Ramos <126737369+guilhermelimaramos@users.noreply.github.com>
2023-03-03 10:29:33 -0300

    Novos exercicios

---
+++ ex002/tratamento de dados.js
@@ -0,0 +1,10 @@
+var nome = window.prompt('Qual o seu Nome?')
+window.alert('Bem-Vindo!, ' + nome)
+
+var n1 = Number(prompt('Digite um número:'))
+var n2 = Number(prompt('Digite outro número:'))
+var soma = n1 + n2
+var sub = n1 - n2
+
+alert('A soma de ' + String(n1) + ' + ' + String(n2) + ' = ' + soma)
+alert(`A subtração de ${n1} + ${n2} = ${sub}`)
---
+++ ex004/tratamento de dados pt.2.js
@@ -0,0 +1,4 @@
+var nome = String(prompt('Digite se Nome:'))
+document.write(` Olá <strong>${nome}</strong>, Seu nome tem ${nome.length} letras <br>`)
+document.write(`Seu nome em maiúsculas fica <strong>${nome.toUpperCase()}</strong> <br>`)
+document.write(`E seu nome em minúsculas fica <strong>${nome.toLowerCase()}`)
