469c20957f4eaa10050595b8560128db92e63de3
Guilherme Lima Ramos <126737369+guilhermelimaramos@users.noreply.github.com>
2023-03-04 14:37:27 -0300

    novos exercicios

--- exercícios/ex004/tratamento de dados pt.2.js
+++
@@ -1,4 +0,0 @@
-var nome = String(prompt('Digite se Nome:'))
-document.write(` Olá <strong>${nome}</strong>, Seu nome tem ${nome.length} letras <br>`)
-document.write(`Seu nome em maiúsculas fica <strong>${nome.toUpperCase()}</strong> <br>`)
-document.write(`E seu nome em minúsculas fica <strong>${nome.toLowerCase()}`)
--- exercícios/ex009/condição composta.js
+++
@@ -1,9 +0,0 @@
-var pais = 'China'
-
-console.log(`Vivendo no(a) ${pais}`)
-
-if (pais == 'Brasil') {
-    console.log('Você é Brasileiro')
-} else {
-    console.log('Você é Gringo')
-}
