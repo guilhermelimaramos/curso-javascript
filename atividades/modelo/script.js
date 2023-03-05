c94a8bb5d6379dd147be3609ce403923beaf29e5
Guilherme Lima Ramos <126737369+guilhermelimaramos@users.noreply.github.com>
2023-03-05 13:58:26 -0300

    nova atividade

---
+++ atividades/modelo/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = document.querySelector('#msg')
+    var img = document.querySelector('#foto')
+    var data = new Date()
+    var hora = data.getHours()
+    var hora = 20
+
+    msg.innerHTML = `Agora são ${hora} hora(s)`
+
+    if (hora < 12) {
+        //BOM DIA
+        img.src = 'manhã.png'
+    } else if (hora < 18) {
+        //BOA TARDE
+        img.src = 'tarde.png'
+    } else {
+        //BOA NOITE
+        img.src = 'noite.png'
+    }
+}
