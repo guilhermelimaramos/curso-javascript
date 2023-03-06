fae0b16945586ad07a2414cb170cb7244aee094f
Guilherme Lima Ramos <126737369+guilhermelimaramos@users.noreply.github.com>
2023-03-06 17:37:28 -0300

    novo exercício

---
+++ atividades/node_1c0b7c00bedd6.tmp.js
@@ -0,0 +1,15 @@
+/*var cont = 1;
+while (cont < 7) {
+    console.log(`Passo ${cont}`)
+    cont++
+}
+
+do {
+    console.log(`Passo ${cont}`)
+    cont++
+} while (cont < 7) 
+*/
+
+for (var cont = 1; cont < 11; 2) {
+    console.log(`Passo ${cont}`)
+}
